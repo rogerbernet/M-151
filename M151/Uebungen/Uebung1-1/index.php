5000bae14702f6d165ad1dde2164282090b5f6d6
vmadmin <vmadmin@webvm-wan.localdomain>
2022-02-03 10:35:37 +0100

    Uebung1-1

---
+++ M151/Uebungen/Uebung1-1/index.php
@@ -0,0 +1,14 @@
+<?php
+phpinfo();
+?>
+
+<!--
+php -S 0.0.0.0:8000
+
+Zu sehen sind die Server-php spezifikationen 
+
+-S steht für server oder serve
+
+0.0.0.0 sind die zuhörenden IP adresse in unserem fall steht 0.0.0.0 für alle ":8000" sthet für den Port 8000
+
+-->
